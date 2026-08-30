d52abfb9106ba72311ef96efffa21f8fa220643d
banukadilshan-dissanayaka <banukadilshan078@gmail.com>
2026-08-30 14:40:31 +0530

    Register EATMO menu schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import type { SchemaTypeDefinition } from "sanity";
+
+import menuCategory from "./menuCategory";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [menuCategory],
+};
