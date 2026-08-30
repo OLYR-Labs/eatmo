e9829151a0f7851f86ee214a6c157fd60fdba3c0
banukadilshan-dissanayaka <banukadilshan078@gmail.com>
2026-08-30 15:34:22 +0530

    Show Menu in Sanity Studio navigation

--- sanity/structureConfig.ts
+++ sanity/structureConfig.ts
@@ -3,4 +3,6 @@ import type { StructureResolver } from "sanity/structure";
 export const structure: StructureResolver = (S) =>
   S.list()
     .title("Content")
-    .items(S.documentTypeListItems());
+    .items([
+      S.documentTypeListItem("menuCategory").title("Menu"),
+    ]);
