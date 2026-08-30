aa1edd167d2ec815205977daa4b3ef7bf85b2e6a
banukadilshan-dissanayaka <banukadilshan078@gmail.com>
2026-08-30 15:39:58 +0530

    Remove Sanity schema index

--- sanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import type { SchemaTypeDefinition } from "sanity";
-
-import menuCategory from "./menuCategory";
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [menuCategory],
-};
