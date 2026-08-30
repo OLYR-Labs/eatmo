3b4cf319751212cd7d7507e0b2f765c4af0fdb77
banukadilshan-dissanayaka <banukadilshan078@gmail.com>
2026-08-30 15:39:30 +0530

    Remove Sanity Studio route

--- app/studio/[[...tool]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import StudioClient from "@/components/StudioClient";
-
-export const dynamic = "force-static";
-
-export { metadata, viewport } from "next-sanity/studio";
-
-export default function StudioPage() {
-  return <StudioClient />;
-}
