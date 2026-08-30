a4cac53992abe8d35b1911b0b796619e91aa4100
banukadilshan-dissanayaka <banukadilshan078@gmail.com>
2026-08-30 15:09:12 +0530

    Remove Vision plugin from embedded Studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,10 +1,9 @@
 'use client'
 
-import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 
-import { apiVersion, dataset, projectId } from "./sanity/env";
+import { dataset, projectId } from "./sanity/env";
 import { schema } from "./sanity/schemaTypes";
 import { structure } from "./sanity/structureConfig";
 
@@ -15,6 +14,5 @@ export default defineConfig({
   schema,
   plugins: [
     structureTool({ structure }),
-    visionTool({ defaultApiVersion: apiVersion }),
   ],
 });
