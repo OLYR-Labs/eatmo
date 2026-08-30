0e5c394f794706f0a62d1c35539f9ddad81c6f13
banukadilshan-dissanayaka <banukadilshan078@gmail.com>
2026-08-30 15:39:43 +0530

    Remove Sanity structure configuration

--- sanity/structureConfig.ts
+++
@@ -1,8 +0,0 @@
-import type { StructureResolver } from "sanity/structure";
-
-export const structure: StructureResolver = (S) =>
-  S.list()
-    .title("Content")
-    .items([
-      S.documentTypeListItem("menuCategory").title("Menu"),
-    ]);
